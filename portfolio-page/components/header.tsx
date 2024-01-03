815168244891b7a3ec90661497b45480dcbb2b39
Rashidat <rawsheedah@yahoo.com>
2024-01-03 18:29:33 +0000

    Refactor activeSection when in view

--- portfolio-page/components/header.tsx
+++ portfolio-page/components/header.tsx
@@ -8,7 +8,7 @@ import clsx from 'clsx';
 import { useActiveSectionContext } from '@/context/active-section-context';
 
 export default function Header() {
-  const { activeSection, setActiveSection } = useActiveSectionContext();
+  const { activeSection, setActiveSection, setTimeOfLastClick } = useActiveSectionContext();
 
   return (
     <header className='z-[999] relative'>
@@ -28,10 +28,14 @@ export default function Header() {
                 animate={{ y: 0, opacity: 1 }}>
 
                 <Link
-                className={clsx('flex w-full items-center justify-center px-3  py-3 hover:text-gray-950 transition', {
+                className={clsx(
+                  'flex w-full items-center justify-center px-3  py-3 hover:text-gray-950 transition', {
                   'text-gray-950': activeSection === link.name, })} 
                 href={link.hash}
-                onClick={() => setActiveSection(link.name)}
+                onClick={() => { 
+                  setActiveSection(link.name);
+                  setTimeOfLastClick(Date.now());
+                }}
                 >
                   {link.name}
 
