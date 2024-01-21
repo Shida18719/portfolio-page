90b5bb3e734b433cdaa5c64d9ebffe2848d6ce0e
Rashidat <rawsheedah@yahoo.com>
2024-01-21 16:43:42 +0000

    Modify style

--- portfolio-page/components/project.tsx
+++ portfolio-page/components/project.tsx
@@ -28,22 +28,25 @@ export default function Project({ title, description, tags, imageUrl }: ProjectP
     >
 
     <section className='group bg-gray-100 max-w-[42rem] border border-black/5 overflow-hidden
-        rounded-lg sm:pr-8 relative sm:h-[20rem] group-even:pl-8 hover:bg-gray-200 transition'>
+        rounded-lg sm:pr-8 relative sm:h-[20rem] sm:group-even:pl-8 hover:bg-gray-200 transition dark:bg-white/10 
+        dark:text-white dark:hover:bg-white/20'>
             <div className='pt-4 pb-7 px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%] 
-                flex flex-col h-full group-even:ml-[18rem]'>
+                flex flex-col h-full sm:group-even:ml-[18rem]'>
                 <h3 className='text-2xl font-semibold'>{title}</h3>
-                <p className='mt-2 leading-relaxed text-gray-700'>{description}</p>
+                <p className='mt-2 leading-relaxed text-gray-700 dark:text-white/70'>
+                  {description}</p>
                 <ul className='flex flex-wrap mt-4 gap-2 sm:mt-auto'>
                     {tags.map((tag, index) => (
                         <li className='bg-black/[0.7] px-3 py-1 text-[0.7rem] uppercase 
-                        tracking-wider text-white rounded-full' 
-                        key={index}>{tag}</li>
+                          tracking-wider text-white rounded-full dark:text-white/70' 
+                          key={index}>{tag}
+                        </li>
                     ))}
                 </ul>
             </div>
 
             <Image src={imageUrl} alt="Portfolio Projects" quality={95} 
-            className='absolute -right-40 top-8 w-[28.25rem] rounded-t-lg shadow-2xl
+            className='absolute hidden sm:block -right-40 top-8 w-[28.25rem] rounded-t-lg shadow-2xl
             transition group-hover:scale-[1.04]
             group-hover:-translate-x-3 group-hover:translate-y-3
             group-hover:-rotate-2
