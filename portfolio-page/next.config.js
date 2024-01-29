7a98b7256a7be1fb31772d2a4ff7c89dac628889
Rashidat <rawsheedah@yahoo.com>
2024-01-29 11:44:31 +0000

    Ammend error in nextConfig

--- portfolio-page/next.config.js
+++ portfolio-page/next.config.js
@@ -1,10 +1,10 @@
 /** @type {import('next').NextConfig} */
 
-// const nextConfig = {
+const nextConfig = {
 
-//     experimental: {
-//     serverActions: true,
-//     }
-// };
+    // experimental: {
+    // serverActions: true,
+    // }
+};
 
-// module.exports = nextConfig;
+module.exports = nextConfig;
